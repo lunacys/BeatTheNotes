1c82f2c13f2ba200450ebaeda2e580213b22b4d3
Grigory Alfyorov <grishafof@gmail.com>
2018-04-11 20:37:35 +0500

    Update Assembly info

--- BeatTheNotes.DesktopGL/Properties/AssemblyInfo.cs
+++ BeatTheNotes.DesktopGL/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Jackhammer.DesktopGL")]
-[assembly: AssemblyProduct("Jackhammer.DesktopGL")]
+[assembly: AssemblyTitle("Beat The Notes")]
+[assembly: AssemblyProduct("Beat The Notes")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © Grigory 'loonacuse' Alfyorov 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,3 +34,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
+
