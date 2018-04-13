a4757b60eb7c3667a07b6fd3dab2fabe19aabcb1
Grigory Alfyorov <grishafof@gmail.com>
2018-04-13 09:09:42 +0500

    Add spawner and Mine

---
+++ BeatTheNotes.Framework/Objects/HitObjectSpawner.cs
@@ -0,0 +1,12 @@
+﻿namespace BeatTheNotes.Framework.Objects
+{
+    public class HitObjectSpawner
+    {
+
+
+        public HitObject Spawn()
+        {
+            return new NoteClick(1, 0);
+        }
+    }
+}
