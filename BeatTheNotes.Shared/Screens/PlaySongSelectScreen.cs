8d49ce71866a088fc08167335899197d38ebe160
lunacys <grishafof@gmail.com>
2018-04-18 14:44:00 +0500

    Start working on PlaySongSelectScreen

--- BeatTheNotes.Shared/Screens/PlaySongSelectScreen.cs
+++ BeatTheNotes.Shared/Screens/PlaySongSelectScreen.cs
@@ -1,13 +1,28 @@
-﻿using Microsoft.Xna.Framework;
+﻿using BeatTheNotes.Framework.Beatmaps;
+using BeatTheNotes.Framework.Settings;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using MonoGame.Extended.Screens;
 
 namespace BeatTheNotes.Screens
 {
     public class PlaySongSelectScreen : Screen
     {
+        private Game _game;
+        private BeatmapProcessor _beatmapProcessor;
+        private SpriteBatch _spriteBatch;
+
+        public PlaySongSelectScreen(Game game)
+        {
+            _game = game;
+            _beatmapProcessor = new BeatmapProcessor(game.Services.GetService<GameSettings>());
+        }
+
         public override void Initialize()
         {
+            base.Initialize();
 
+            _spriteBatch = new SpriteBatch(_game.GraphicsDevice);
         }
 
         public override void LoadContent()
@@ -22,7 +37,9 @@ namespace BeatTheNotes.Screens
 
         public override void Draw(GameTime gameTime)
         {
+            _spriteBatch.Begin();
 
+            _spriteBatch.End();
         }
     }
 }
