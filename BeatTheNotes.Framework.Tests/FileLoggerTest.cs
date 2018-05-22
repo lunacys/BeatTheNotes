ea23999f223d317818beb5a315a779dcef68273c
Grigory Alfyorov <grishafof@gmail.com>
2018-05-22 21:20:51 +0500

    Add FileLogger and incomplete LogHelper tests

---
+++ BeatTheNotes.Framework.Tests/FileLoggerTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using BeatTheNotes.Framework.Debugging.Logging;
+using NUnit.Framework;
+
+namespace BeatTheNotes.Framework.Tests
+{
+    [TestFixture]
+    public class FileLoggerTest
+    {
+        [Test]
+        public void TestFileLoggerInitialization()
+        {
+            FileLogger fileLogger = new FileLogger();
+
+            Assert.That(fileLogger.FileDir, Is.Not.Empty);
+            Assert.That(fileLogger.FileDir, Is.Not.Null);
+            Assert.That(fileLogger.FilePath, Is.Not.Empty);
+            Assert.That(fileLogger.FilePath, Is.Not.Null);
+
+            Assert.AreEqual(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Logs"), fileLogger.FileDir);
+            Assert.AreEqual(
+                Path.Combine(fileLogger.FileDir,
+                    $"log-{DateTime.Now.Month}.{DateTime.Now.Day}.{DateTime.Now.Year}.txt"), fileLogger.FilePath);
+        }
+
+        [Test]
+        public void TestFileLoggerLogging()
+        {
+            FileLogger fileLogger = new FileLogger();
+
+            fileLogger.Log("Test Log Message");
+
+            string message;
+
+            using (StreamReader reader = new StreamReader(fileLogger.FilePath))
+                message = reader.ReadLine();
+
+            Assert.AreEqual("Test Log Message", message);
+        }
+    }
+}
---
+++ BeatTheNotes.Framework.Tests/LogHelperTests.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using BeatTheNotes.Framework.Debugging.Logging;
+using NUnit.Framework;
+
+namespace BeatTheNotes.Framework.Tests
+{
+    [TestFixture]
+    public class LogHelperTests
+    {
+        [Test]
+        public void TestLogHelperInitialization()
+        {
+            LogHelper.Target = LogTarget.File;
+
+            for (int i = 0; i < 10; i++)
+            {
+                LogHelper.Log($"Hello from the Test! {i}", LogLevel.Info);
+                LogHelper.Update();
+            }
+
+            // TODO: This
+        }
+    }
+}
